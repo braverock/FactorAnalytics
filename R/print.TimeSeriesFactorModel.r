62ca40c891db17f13fef7b389c4d3c0462d5bbf5
Yian Yi-An Chen <chenyian@uw.edu>
2013-07-25 18:18:00 +0000

    create print.TimeSeriesFactorModel.r and print.TimeSeriesFactorModel.Rd
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/FactorAnalytics@2644 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/print.TimeSeriesFactorModel.r
+++ R/print.TimeSeriesFactorModel.r
@@ -4,20 +4,26 @@
 #' 
 #' 
 #' @param fit.macro fit object created by fitTimeSeriesFactorModel.
+#' @param digits. integer indicating the number of decimal places.  
 #' @author Eric Zivot and Yi-An Chen.
 #' @examples
 #' 
 #' # load data from the database
 #' data(managers.df)
-#' ret.assets = managers.df[,(1:6)]
-#' factors    = managers.df[,(7:9)]
-#' # fit the factor model with OLS
-#' fit.macro <- fitTimeSeriesFactorModel(ret.assets,factors,fit.method="OLS",
-#'                                  variable.selection="all subsets")
+#' fit.macro <- fitTimeseriesFactorModel(assets.names=colnames(managers.df[,(1:6)]),
+#'                                       factors.names=c("EDHEC.LS.EQ","SP500.TR"),
+#'                                       data=managers.df,fit.method="OLS")
 #' print(fit.macro)
 #' 
-#' 
-print.TimeSeriesFactorModel <-
-  function(fit.macro) {
-    lapply(fit.macro[[1]], print)
-  }
+#' @export
+print.TimeSeriesFactorModel <- function(fit.macro,digits=3){
+n <- length(fit.macro$beta)
+table.macro <-  as.matrix(fit.macro$alpha,nrow=n[1])
+table.macro <- cbind(table.macro,fit.macro$beta,fit.macro$r2,fit.macro$resid.variance)
+beta.names <- colnames(fit.macro$beta)
+for (i in 1:length(beta.names)) {
+beta.names[i] <- paste("beta.",beta.names[i],sep="")
+}
+colnames(table.macro) <- c("alpha",beta.names,"r2","resid.var")
+print(round(table.macro,digits=digits))
+}
