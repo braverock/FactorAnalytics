66af10077f85605a35513a84117df885b64bf150
Yian Yi-An Chen <chenyian@uw.edu>
2013-08-02 22:32:44 +0000

    modify .Rd file for the change of VaR.method
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/FactorAnalytics@2708 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/factorModelSdDecomposition.R
+++ R/factorModelSdDecomposition.R
@@ -19,34 +19,22 @@
 #' @examples
 #' 
 #' # load data from the database
-#' data(managers.df)
-#' ret.assets = managers.df[,(1:6)]
-#' factors    = managers.df[,(7:9)]
-#' # fit the factor model with OLS
-#' fit <- fitMacroeconomicFactorModel(ret.assets,factors,fit.method="OLS", 
-#'                                  variable.selection="all subsets",
-#'                                  factor.set = 3)
-#' # factor SD decomposition for HAM1
-#' cov.factors = var(factors)
-#' manager.names = colnames(managers.df[,(1:6)])
-#' factor.names  = colnames(managers.df[,(7:9)])
-#' factor.sd.decomp.HAM1 = factorModelSdDecomposition(fit$beta.mat["HAM1",],
-#'                                                    cov.factors, fit$residVars.vec["HAM1"])
+#' data("stat.fm.data")
+#' fit.stat <- fitStatisticalFactorModel(sfm.dat,k=2)
+#' cov.factors = var(fit.stat$factors)
+#' names = colnames(fit.stat$asset.ret)
+#' factor.sd.decomp.list = list()
+#' for (i in names) {
+#'  factor.sd.decomp.list[[i]] =
+#'    factorModelSdDecomposition(fit.stat$loadings[,i],
+#'                               cov.factors, fit.stat$resid.variance[i])
+#' }
 #'  
 #' @export                                       
 #' 
 factorModelSdDecomposition <-
 function(beta.vec, factor.cov, sig2.e) {
-## Inputs:
-## beta   		   k x 1 vector of factor betas with factor names in the rownames
-## factor.cov		 k x k factor excess return covariance matrix 
-## sig2.e			   scalar, residual variance from factor model (residVars.vec in fitFundamentalFactorModel)
-## Output:
-## A list with the following components:
-## sd.fm              scalar, std dev based on factor model
-## mcr.fm             k+1 x 1 vector of factor marginal contributions to risk (sd)
-## cr.fm              k+1 x 1 vector of factor component contributions to risk (sd)
-## pcr.fm             k+1 x 1 vector of factor percent contributions to risk (sd)
+
 ## Remarks:
 ## The factor model has the form
 ## R(t) = beta'F(t) + e(t) = beta.star'F.star(t)
