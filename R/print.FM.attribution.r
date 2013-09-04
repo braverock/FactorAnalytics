20d5684977f7e528dd8ff5ec981a52f1397818e1
Yian Yi-An Chen <chenyian@uw.edu>
2013-09-04 00:23:36 +0000

    add new function print.FM.attribution.r and print.FM.attribution.Rd
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/FactorAnalytics@2982 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/print.FM.attribution.r
@@ -0,0 +1,28 @@
+#' Print FM.attribution object.
+#' 
+#' Generic function of print method for factorModelPerformanceAttribution.
+#' 
+#' 
+#' @param fm.attr FM.attribution object created by
+#' factorModelPerformanceAttribution.
+#' @author Yi-An Chen.
+#' @examples
+#' \dontrun{
+#' # load data from the database
+#'  data(managers.df)
+#'  # fit the factor model with OLS
+#'  fit.ts <- fitTimeSeriesFactorModel(assets.names=colnames(managers.df[,(1:6)]),
+#'                                factors.names=c("EDHEC.LS.EQ","SP500.TR"),
+#'                                data=managers.df,fit.method="OLS")
+#'   
+#'   fm.attr <- factorModelPerformanceAttribution(fit.ts)
+#'   print(fm.attr)
+#'   }
+#' @method print FM.attribution  
+#' @export   
+#' 
+print.FM.attribution <- function(fm.attr) {
+  cat("\nMean of returns attributed to factors
+      \n")
+  print(sapply(fm.attr[[3]],function(x) apply(x,2,mean)))
+ }
