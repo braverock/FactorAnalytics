596ef96801fc027fa2dfed309587a81eb93eeb7c
Avinash Acharya <acharya@uw.edu>
2016-06-25 09:28:52 -0700

    Roxygen comments added for portRsq.R

--- R/portRsq.R
+++ R/portRsq.R
@@ -2,18 +2,31 @@
 #' 
 #' @description Calcluate the R-squared and Adjusted R-squared for the portfolio of stocks
 #' 
+#' @param z  an object of class \code{ffm} produced by \code{fitFfm}
+#' @param weight a vector of weights of the assets in the portfolio. Default is NULL.
+#' @param ... additional arguments unused
+#' @author Avinash Acharya
+#' @examples 
+#'
+# #Load the data 
+#' data("stocks145scores6")
+#'  
+#' #Fit a Ffm
+#' fit <- fitFfm(data=stock, asset.var="TICKER", ret.var="RETURN", 
+#'               date.var="DATE", exposure.vars="SECTOR")
+#' #Find the portfolio R-squared and adjusted portfolio R-squared with default weights.               
+#' portRsq(fit)
 #' 
-#' 
+#' @export
+
 # Not the final version
-portRsq <- function(object, weight=NULL, ...)
+portRsq <- function(z, weight=NULL, ...)
 {
   # set defaults and check input validity
   if (!inherits(object, "ffm")) {
     stop("Invalid argument: Object should be of class'ffm'.")
   }
   
-  
-  z<- object
   data <- z$data
   date.var = z$date.var
   data <- data[order(data[,date.var]),]
