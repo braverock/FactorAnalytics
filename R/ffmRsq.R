ee6d2ee2eb20ada7151c01eb1033241a7dea55cc
Avinash Acharya <acharya@uw.edu>
2016-08-03 16:18:22 -0700

    Plot style changed to have ZeroLine

--- R/ffmRsq.R
+++ R/ffmRsq.R
@@ -22,7 +22,7 @@
 #' @param title    logical. if \code{TRUE}, the plots will have the main tiltle. default is \code{TRUE}.
 #' 
 #' @param ...      potentially further arguments passed.
-#' @author Doug Martin, Avinash Acharya
+#' @author Avinash Acharya and Doug Martin
 #'
 #' @return \code{ffmRsq} returns the sample mean values and plots the time series of corresponding R squared values
 #'                         and the Variance Inflation factors depending on the values of \code{rsq}, \code{rsqAdj} and \code{VIF}.
@@ -48,7 +48,7 @@
 #'  ffmRsq(fit1, VIF=TRUE, rsqAdj=TRUE, isPrint=TRUE, plt.type = 2)
 #' @export
 
-# Not the final version
+
 ffmRsq <- function(ffmObj, rsq=T, rsqAdj=F, VIF=F, plt.type= 2, digits=2, isPrint=T, isPlot =T, lwd =2, title = TRUE, ...)
 {
   # set defaults and check input validity
@@ -71,22 +71,28 @@ ffmRsq <- function(ffmObj, rsq=T, rsqAdj=F, VIF=F, plt.type= 2, digits=2, isPrin
   if(rsq)
   {
     if(isPlot && plt.type == 1)
-      {
-        barplot(r2,las=2,col=5,
-                names.arg= as.yearmon(names(r2)),
-                cex.names=0.5,
-                main=" ")
+    {
+      barplot(r2,las=2,col=5,
+              names.arg= as.yearmon(names(r2)),
+              cex.names=0.5,
+              main=" ")
       if(title){title("Factor Model R-squared Values")}
     }
     else if (isPlot && plt.type == 2)
-      {
+    {
       r2.xts = xts(r2, order.by = as.yearmon(names(r2)))
+      
       if(rsqAdj) plt.r2 = TRUE else{
         if(title) title.Rsq = "Factor Model R-squared Values" else title.Rsq = " " 
-        tsPlotMP(0.01*r2.xts,stripLeft = FALSE, scaleType = "same",
-                 color = "blue", yname = "",lwd =lwd,  main = title.Rsq, type = "h")
-          }
+        panel =  function(...){
+          panel.abline(h=0,lty = 3)
+          panel.xyplot(...)}
+        plt = xyplot(r2.xts,col = "blue",lwd =lwd, panel =panel, scale = list(y = list(cex=1,rot =0),x = list(cex =1)),
+                     main = title.Rsq, type = "h", ylim = c(0,(max(r2.xts)+0.05)), strip.left = strip.custom(var.name = "R-Squared", style = 1, strip.names = T,strip.levels=F ))
+        print(plt) 
+        
       }
+    }
     r2.mean<- round(mean(r2),digits = digits)
     names(r2.mean) <- "Mean R-Squared"
     out<- r2.mean
@@ -102,16 +108,21 @@ ffmRsq <- function(ffmObj, rsq=T, rsqAdj=F, VIF=F, plt.type= 2, digits=2, isPrin
     if(isPlot && plt.type == 1)
     {
       barplot(adj.r2,las=2,col=5,
-            names.arg= as.yearmon(names(r2)),
-            cex.names=0.5,
-            main=" ")
+              names.arg= as.yearmon(names(r2)),
+              cex.names=0.5,
+              main=" ")
       if(title){title(" Factor Model Adjusted R-squared Values")}
     }
     else if (isPlot && plt.type == 2 && !plt.r2)
     {
       if(title) title.AdjRsq = "Factor Model Adjusted R-squared Values" else title.AdjRsq = " " 
-      tsPlotMP(0.01*adj.r2.xts,stripLeft = FALSE, scaleType = "same",
-               color = "blue", yname = "", lwd = lwd, main = title.AdjRsq, type = "h")
+      panel =  function(...){
+        panel.abline(h=0,lty = 3)
+        panel.xyplot(...)}
+      plt = xyplot(adj.r2.xts,col = "blue", lwd =lwd, main = title.AdjRsq, type = "h",panel = panel,
+                   scales = list(y = list(cex = 1,relation="same"),x = list(cex =1)),
+                   strip.left = strip.custom(var.name = "Adj R-Squared", style = 1, strip.names = T,strip.levels=F ))
+      print(plt) 
     }
     adj.r2.mean<- round(mean(adj.r2),digits = digits)
     names(adj.r2.mean) <- "Mean Adj R-Squared"
@@ -124,7 +135,7 @@ ffmRsq <- function(ffmObj, rsq=T, rsqAdj=F, VIF=F, plt.type= 2, digits=2, isPrin
     if (isPlot && plt.type == 2)
     { 
       if(title) title.comb = "Factor Model R-squared Values" else title.comb = " " 
-      r2.combined = merge.xts("R-squared" = r2.xts,"Adjusted R-squared" =  adj.r2.xts)
+      r2.combined = merge.xts("R-squared" = r2.xts,"Adj R-squared" =  adj.r2.xts)
       tsPlotMP(0.01*r2.combined,stripLeft = TRUE, scaleType = "same",
                color = "blue", yname = "", lwd = lwd, main = title.comb, type = "h", cex = 1.2)
       
@@ -138,42 +149,42 @@ ffmRsq <- function(ffmObj, rsq=T, rsqAdj=F, VIF=F, plt.type= 2, digits=2, isPrin
     which.numeric <- sapply(ffmObj$data[,exposure.vars,drop=FALSE], is.numeric)
     exposures.num <- exposure.vars[which.numeric]
     if(length(exposures.num) < 2)
-      {
-        stop(" At least 2 continous variables required to find VIF")
-      }
-
+    {
+      stop(" At least 2 continous variables required to find VIF")
+    }
+    
     object = ffmObj$data[exposures.num]  
     object <- as.matrix(object)
     ncols <- dim(object)[2]
     time.periods = length(ffmObj$time.periods)
     vifs = matrix(0, nrow = time.periods, ncol = ncols)
     for(i in 1:60)
-      {
+    {
       vifs[i,1:ncols] = sapply(seq(ncols), function(x)
-                                1/(1 - summary(lm(object[((i-1)*n.assets+1) : (i*n.assets), x] ~ 
-                                                  object[((i-1)*n.assets+1) :(i*n.assets), -x]))$r.squared))
-      }
+        1/(1 - summary(lm(object[((i-1)*n.assets+1) : (i*n.assets), x] ~ 
+                            object[((i-1)*n.assets+1) :(i*n.assets), -x]))$r.squared))
+    }
     colnames(vifs) <- dimnames(object)[[2]]
     vifs.xts = xts(vifs, order.by = ffmObj$time.periods)
     vifs.mean = round(colMeans(vifs.xts),digits = digits)
     if(isPlot)
     {
       if(title) title.vif = "Factor Model VIF Values" else title.vif = " " 
-    #Assuming the number of continous variables in exposure.vars is less than 6,layout=c(1,ncols) is defined.
+      #Assuming the number of continous variables in exposure.vars is less than 6,layout=c(1,ncols) is defined.
       tsPlotMP(0.01*vifs.xts,stripLeft = TRUE, layout = c(1,ncols), scaleType = "same",
-             color = "blue", yname = "", lwd = lwd, main =title.vif, type = "h")
+               color = "blue", yname = "", lwd = lwd, main =title.vif, type = "h")
     }
     vifs.xts = round(vifs.xts,digits = digits)
     out<- append(out, list("Mean.VIF" = vifs.mean))
     ret<- append(ret, list("VIF" = vifs.xts))
   }
-
+  
   if(isPrint)
-    {
-      print(c(out, ret))
-    }else{
-      print(out)
-      invisible(c(out, ret))
-    }
+  {
+    print(c(out, ret))
+  }else{
+    print(out)
+    invisible(c(out, ret))
+  }
 }
 
