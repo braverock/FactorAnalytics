6f3977424228567af45bbe1dccd1d5662766b093
Avinash Acharya <acharya@uw.edu>
2016-12-03 19:04:07 -0800

    Build errors removed

--- R/assetDecomp.R
+++ R/assetDecomp.R
@@ -16,8 +16,9 @@
 #'
 #' @return Risk Decomposition report for every asset in the portfolio
 #' 
-#' @author Avinash 
-#' 
+#' @author Avinash Acharya
+#' @references 
+#' Epperlein and Smillie (2006) "Cracking VAR with Kernels," Risk.net
 #' 
 #' @seealso \code{\link{riskDecomp}}
 #' for the Risk Decomposition function based on factors in the fitted model.
@@ -26,22 +27,32 @@
 #' @examples
 #' # Fundamental Factor Model
 #'data("factorDataSetDjia5Yrs")
+#'data("wtsDjiaGmvLo")
+#'
 #' # fit a fundamental factor model
 #'exposure.vars <- c("P2B", "MKTCAP")
 #'fit <- fitFfm(data=factorDataSetDjia5Yrs, asset.var="TICKER", ret.var="RETURN", 
 #'                 date.var="DATE", exposure.vars=exposure.vars,z.score = TRUE )
+#'                 
+#'#non-parametric 
+#'weights = wtsDjiaGmvLo               
 #'sd.decomp = assetDecomp(fit, weights, rm = "Sd")
 #'VaR.decomp = assetDecomp(fit, weights, rm = "VaR", p = 0.05)
 #'ES.decomp = assetDecomp(fit, weights, rm = "ES", p = 0.05)                 
 #'
+#'#normal dist
+#'VaR.decomp = assetDecomp(fit, weights, rm = "VaR", p = 0.05, type = "normal")
+#'ES.decomp = assetDecomp(fit, weights, rm = "ES", p = 0.05, type = "normal") 
 #' @export   
  
-assetDecomp = function(object,rm, weights, p, type = "np")
+assetDecomp = function(object, weights=NULL, rm, p, type = c("np", "normal"))
 {
+  type = type[1]
   n.assets = length(object$asset.names)
   returns = matrix(object$data[,"RETURN"], n.assets,length(object$time.periods))
   returns = xts(t(returns), order.by =unique(object$data[,"DATE"] ))
-  weights = matrix(rep(1/n.assets, n.assets), ncol = 1)
+  if(is.null(weights)) {weights = matrix(rep(1/n.assets, n.assets), ncol = 1)}
+  else weights = matrix(weights, ncol = 1)
   port.Sd = sqrt(t(weights) %*% cov(returns) %*% weights)
   switch(rm,
          Sd = 
@@ -65,17 +76,17 @@ assetDecomp = function(object,rm, weights, p, type = "np")
 
            else if(type == "np")
            {
-             R.xts = returns %*% weights
-             R.xts = xts(R.xts, order.by = index(returns))
-             Risk.fm <- quantile(R.xts, probs=p, na.rm=TRUE)
+             portRet = returns %*% weights
+             portRet = xts(portRet, order.by = index(returns))
+             Risk.fm <- quantile(portRet, probs=p, na.rm=TRUE)
              # epsilon is apprx. using Silverman's rule of thumb (bandwidth selection)
              # the constant 2.575 corresponds to a triangular kernel 
-             eps <- 2.575*sd(R.xts, na.rm =TRUE) * (nrow(R.xts)^(-1/5))
+             eps <- 2.575*sd(portRet, na.rm =TRUE) * (nrow(portRet)^(-1/5))
              
              # compute marginal VaR as expected value of factor returns, such that the
              # asset return was incident in the triangular kernel region peaked at the 
              # VaR value and bandwidth = epsilon.
-             k.weight <- as.vector(1 - abs(R.xts - Risk.fm) / eps)
+             k.weight <- as.vector(1 - abs(portRet - Risk.fm) / eps)
              k.weight[k.weight<0] <- 0
              mRisk <- colMeans(returns*k.weight, na.rm =TRUE)
 
@@ -93,23 +104,24 @@ assetDecomp = function(object,rm, weights, p, type = "np")
           }
            else if(type == "np")
            {
-             
              VaR.fm <- rep(NA, 1)
-             # get VaR for asset i
-             VaR.fm <- quantile(R.xts, probs=p, na.rm=TRUE, ...)
+             portRet = returns %*% weights
+             portRet = xts(portRet, order.by = index(returns))
+             # get VaR for each asset 
+             VaR.fm <- quantile(portRet, probs=p, na.rm=TRUE)
              # index of VaR exceedances
-             idx.exceed <- which(R.xts <= VaR.fm)
+             idx.exceed <- which(portRet <= VaR.fm)
              # compute ES as expected value of asset return, such that the given asset 
              # return is less than or equal to its value-at-risk (VaR)
-             Risk.fm <- mean(R.xts[idx.exceed], na.rm =TRUE)
+             Risk.fm <- mean(portRet[idx.exceed], na.rm =TRUE)
              
              # compute marginal ES as expected value of factor returns, when the asset's 
              # return is less than or equal to its value-at-risk (VaR)
              
-             mRisk <- colMeans(R.xts[idx.exceed,], na.rm =TRUE)       
+             mRisk <- colMeans(portRet[idx.exceed,], na.rm =TRUE)       
            }
          })
-  if(type == "np")
+  if(type == "np" && rm != "Sd")
   {
     # correction factor to ensure that sum(cRisk) = asset Risk
     cf <- as.numeric( Risk.fm / sum(mRisk*weights), na.rm=TRUE) 
