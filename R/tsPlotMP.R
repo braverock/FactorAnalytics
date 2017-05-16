a8b00ec89ffa8a5ade6bfc3b989628d8e8f2e3a0
Avinash Acharya <acharya@uw.edu>
2017-05-16 02:18:45 -0700

    Removed build warnings.

--- R/tsPlotMP.R
+++ R/tsPlotMP.R
@@ -8,10 +8,10 @@
 #' @param ret an time series exposure/return object 
 #' @param add.grid logical varible.If 'TRUE', type = c('l', 'g'); If 'FALSE', type = c('l')
 #' @param layout layout is a numeric vector of length 2 or 3 giving the number of columns, rows, and pages (optional) in a multipanel display.
-#' @param type character. type of the plot; "l" denotes a line, "p" denotes a point, and "b" and "o" both denote both together.deafault is "l".
+#' @param type character. type of the plot; 'l' denotes a line, 'p' denotes a point, and 'b' and 'o' both denote both together.deafault is 'l'.
 #' @param yname character or espression giving label(s) for the y-axis 
 #' @param scaleType scaleType controls if use a same scale of y-axis, choose from c('same', 'free')
-#' @param stripLeft logical variable to choose the position of strip, "TRUE" for drawing strips on the left of each panel, "FALSE" for drawing strips on the top of each panel
+#' @param stripLeft logical variable to choose the position of strip, 'TRUE' for drawing strips on the left of each panel, 'FALSE' for drawing strips on the top of each panel
 #' @param main Typically a character string or expression describing the main title. 
 #' @param lwd The line width, a positive number, defaulting to 1
 #' @param stripText.cex a number indicating the amount by which strip text in the plot(s) should be scaled relative to the default. 1=default, 1.5 is 50\% larger, 0.5 is 50\% smaller, etc.
