6430241af4c7778c431a906b1bbd391104cc09b6
chindhanai <chindhanaipom@gmail.com>
2017-12-23 19:29:36 -0800

    Change the description of cusumData

--- R/datasets.R
+++ R/datasets.R
@@ -112,8 +112,8 @@ NULL
 #' @name cusumData
 #' @title Parvest and Russell2500
 #' @description Data in the example is an xts object containing two monthly returns in each column.
-#' The first column is the fund returns and the second column is the
-#' benchmark returns. The data is from Jan 2005 to Dec 2003.
+#'              The first column is the fund returns and the second column is the
+#'              benchmark returns. The data is from Jan 2005 to Dec 2003.
 #' @docType data
 #' @source TBA
 #' @usage data("cusumData")
