52922a8aba721d39e7c6889151879bfb7a012541
cocok <cocok@192.168.86.45>
2020-01-15 12:43:32 -0800

    add Pct = F argument to tsPlotMP

--- R/tsPlotMP.R
+++ R/tsPlotMP.R
@@ -5,11 +5,12 @@
 #' @importFrom lattice panel.abline xyplot panel.xyplot
 #' @importFrom xts xts
 #' 
-#' @param ret an time series exposure/return object 
+#' @param data an time series exposure/return object 
 #' @param add.grid logical varible.If 'TRUE', type = c('l', 'g'); If 'FALSE', type = c('l')
 #' @param layout layout is a numeric vector of length 2 or 3 giving the number of columns, rows, and pages (optional) in a multipanel display.
 #' @param type character. type of the plot; 'l' denotes a line, 'p' denotes a point, and 'b' and 'o' both denote both together.deafault is 'l'.
 #' @param yname character or espression giving label(s) for the y-axis 
+#' @param Pct Pct controls if use the percentage value. 
 #' @param scaleType scaleType controls if use a same scale of y-axis, choose from c('same', 'free')
 #' @param stripLeft logical variable to choose the position of strip, 'TRUE' for drawing strips on the left of each panel, 'FALSE' for drawing strips on the top of each panel
 #' @param main Typically a character string or expression describing the main title. 
@@ -37,34 +38,37 @@
 #' @export
 
 # Lattice type time series plotting function
-tsPlotMP = function(ret,add.grid = FALSE, layout = NULL,type = "l", 
-                    yname = "RETURNS (%)",scaleType = "free", stripLeft = TRUE,
-                    main = NULL, lwd = 1,stripText.cex =1, axis.cex =1, color = "black", zeroLine = TRUE)
+
+tsPlotMP = function (ret, add.grid = FALSE, layout = NULL, type = "l", yname = "RETURNS (%)", 
+		Pct = F, scaleType = "free", stripLeft = TRUE, main = NULL, lwd = 1, 
+		stripText.cex = 1, axis.cex = 1, color = "black", zeroLine = TRUE) 
 {
-  strip.left = stripLeft
-  strip = !strip.left
-  if(add.grid) {type = c("l","g")} else
-  {type = type}
-  
-  if(zeroLine){
-    panel =  function(...){
-      panel.abline(h=0,lty = 3)
-      panel.xyplot(...)
-    }
-  }else{
-    panel =  function(...){
-      panel.xyplot(...)
-    }
-  }
-
-  pl = xyplot(100*ret,par.strip.text = list(cex = stripText.cex),type = type,
-              xlab="", ylab = list(label = yname,cex = stripText.cex), lwd = lwd,
-              scales = list(y = list(cex = axis.cex,relation=scaleType, rot = 0),
-                            x = list(cex = axis.cex)),layout = layout,main = main,
-              col = color, strip = strip, strip.left = strip.left,
-              panel =  panel)
-  
-  print(pl)
+	strip.left = stripLeft
+	strip = !strip.left
+	if (add.grid) {
+		type = c("l", "g")
+	}
+	else {
+		type = type
+	}
+	if (zeroLine) {
+		panel = function(...) {
+			panel.abline(h = 0, lty = 3)
+			panel.xyplot(...)
+		}
+	}
+	else {
+		panel = function(...) {
+			panel.xyplot(...)
+		}
+	}
+	if(Pct) ret = ret*100
+	pl = xyplot(ret, par.strip.text = list(cex = stripText.cex), 
+			type = type, xlab = "", ylab = list(label = yname, cex = stripText.cex), 
+			lwd = lwd, scales = list(y = list(cex = axis.cex, relation = scaleType, 
+							rot = 0), x = list(cex = axis.cex)), layout = layout, 
+			main = main, col = color, strip = strip, strip.left = strip.left, 
+			panel = panel)
+	print(pl)
 }
-
 
