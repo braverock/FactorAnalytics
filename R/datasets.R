71bc7b90b106380ae4eeef1c032731dca6cb9617
Brian G. Peterson <brian@braverock.com>
2020-11-19 12:28:04 +0000

    add overall package section to documentation, refactor factorAnalytics to FactorAnalytics

--- R/datasets.R
+++ R/datasets.R
@@ -1,13 +1,42 @@
+#' @name FactorAnalytics-package
+#' @aliases FactorAnalytics-package FactorAnalytics
+#'
+#' @description The FactosAnalytics package contains Linear factor model fitting
+#' for asset returns (three major types: time series, fundamental and statistical
+#' factor models); related risk (volatility, VaR and ES) and performance
+#' attribution (factor-contributed vs idiosyncratic returns); tabular displays
+#' of risk and performance reports; factor model Monte Carlo, single and multiple
+#' imputation methods for simulating returns and backfilling unequal histories.
+#'
+#' @section CRSP data included in FactorAnalytics
+#'
+#' FactorAnalytics contains data contributed by the Center for
+#' Research in Security Prices at the University of Chicago's Booth School of
+#' Business (CRSP). The CRSP data is provided for use in the academic hardcopy
+#' and ebook and for examples in the User Manual and Vignettes for, and
+#' containment in, the R package FactorAnalytics. The CRSP-contributed datasets
+#' are not covered by the GPL.  Use of the data in derivative works is not
+#' permitted without the express written permission of CRSP.
+#'
+#' The package developers with to thank S&P Global Markets and the Center for
+#' Research in Security Prices for the generous inclusion of this data in
+#' FactorAnalytics. This data greatly facilitates the research and educational
+#' purpose of FactorAnalytics and allows users of the package to work with a
+#' large dataset that while not independently economically meaningful, is still
+#' large enough to demonstrate model construction of real models.
+#' @docType package
+NULL
+
 #' @name factorDataSPGMI
 #' @title Fundamental factor scores from S&P Global Market Intelligence
 #' @description S&P Global Market Intelligence has kindly provided firm fundamentals data
-#' referred to as scores or alpha factors for educational use in the open source factorAnalytics
+#' referred to as scores or alpha factors for educational use in the open source FactorAnalytics
 #' R package. The data is contained in the R data frame object factorDataSPGMI
 #' consisting of the following cross-section of scores for approximately 300 stocks from 1990 to
 #' 2015: AccrualRatioCF, AnnVol12M, Beta60M, BP, Chg1YEPS, DivP, EBITDAEV, EP, EQ-style,
 #' LogMktCap, PM12M1M, ROE. This data greatly facilitates the educational value to users of the
-#' fundamental factor model in factorAnalytics. The package developers wish to thank S&P Global
-#' Market Intelligence for contributing this data to the factorAnalytics package.
+#' fundamental factor model in FactorAnalytics. The package developers wish to thank S&P Global
+#' Market Intelligence for contributing this data to the FactorAnalytics package.
 #' @docType data
 #' @source S&P Global Market Intelligence
 #' @usage data("factorDataSPGMI")
@@ -18,7 +47,7 @@ NULL
 #' @description Contains returns for 30 DJIA stocks spanned across 9 Sectors -ENERGY, COSTAP, INDUS,T MATRLS,
 #'               FINS, INFOTK, HEALTH, CODISC, and TELCOM stocks along with 4 factor data (MKTCAP, ENTVAL, P2B, EV2S, SIZE)
 #'              starting from Jan 2000 to march 2013.
-#'              
+#'
 #'              The 9 Sectors correspond to Energy,ConsumerStaples, Industrials, Materials, Financials,
 #'              InformationTechnology, HealthCare, ConsumerDiscretionary and Telecommunications respectively.
 #' @docType data
@@ -31,7 +60,7 @@ NULL
 #' @description  Contains returns for 30 DJIA stocks spanned across 9 Sectors -ENERGY, COSTAP, INDUS,T MATRLS,
 #'               FINS, INFOTK, HEALTH, CODISC, and TELCOM stocks along with 4 factor data (MKTCAP, ENTVAL, P2B, EV2S, SIZE)
 #'               starting from  from Jan 2008 to Dec 2012.
-#'               
+#'
 #'               The 9 Sectors correspond to Energy, ConsumerStaples, Industrials, Materials, Financials,
 #'               InformationTechnology, HealthCare, ConsumerDiscretionary and Telecommunications respectively.
 #' @docType data
@@ -42,23 +71,23 @@ NULL
 #' @name stocks145scores6
 #' @title CRSP stocks Capital IQ scores
 #' @description Contains returns for 145 stocks starting from Jan 1990 to Dec 2014 spanned across 10 Sectors-
-#'              ENERGY, COSTAP, INDUS,T MATRLS, FINS, INFOTK, HEALTH, CODISC, UTILS and TELCOM 
+#'              ENERGY, COSTAP, INDUS,T MATRLS, FINS, INFOTK, HEALTH, CODISC, UTILS and TELCOM
 #'              along with 6 factors: ROE, BP, MOM121, SIZE, VOL121, EP
-#'   
+#'
 #'              The 10 Sectors correspond to Energy, ConsumerStaples, Industrials, Materials, Financials,
 #'              InformationTechnology, HealthCare, ConsumerDiscretionary,
 #'              Utilities and Telecommunications respectively.
-#'              
+#'
 #' @docType data
 #' @source TBA
 #' @usage data("stocks145scores6")
-#' 
-#' 
+#'
+#'
 NULL
 
 #' @name wtsDjiaGmv
 #' @title DJIA GMV portfolio weights
-#' @description Contains weights obtained after optimizing the portfolio returns of the 30 DJIA stocks (from dataset factorDataSetDjia5Yrs) 
+#' @description Contains weights obtained after optimizing the portfolio returns of the 30 DJIA stocks (from dataset factorDataSetDjia5Yrs)
 #'              for a global minimum variance portfolio starting from Jan 2008 to Dec 2012.
 #' @docType data
 #' @source TBA
@@ -67,7 +96,7 @@ NULL
 
 #' @name wtsDjiaGmvLo
 #' @title DJIA GMV long-only portfolio weights
-#' @description Contains weights obtained after optimizing the portfolio returns of the 30 DJIA stocks (from dataset factorDataSetDjia5Yrs) 
+#' @description Contains weights obtained after optimizing the portfolio returns of the 30 DJIA stocks (from dataset factorDataSetDjia5Yrs)
 #'              for a long-only global minimum variance portfolio starting from Jan 2008 to Dec 2012.
 #' @docType data
 #' @source TBA
@@ -76,7 +105,7 @@ NULL
 
 #' @name wtsStocks145Gmv
 #' @title CRSP 145 stocks GMV portfolio weights
-#' @description Contains weights obtained after optimizing the portfolio returns of 145 stocks (from dataset stocks145scores6) 
+#' @description Contains weights obtained after optimizing the portfolio returns of 145 stocks (from dataset stocks145scores6)
 #'              for a global minimum variance portfolio starting from Jan 1990 to Dec 2014.
 #' @docType data
 #' @source TBA
@@ -85,7 +114,7 @@ NULL
 
 #' @name wtsStocks145GmvLo
 #' @title CRSP 145 stocks GMV long-only weights
-#' @description Contains weights obtained after optimizing the portfolio returns of 145 stocks (from dataset stocks145scores6) 
+#' @description Contains weights obtained after optimizing the portfolio returns of 145 stocks (from dataset stocks145scores6)
 #'              for a long-only global minimum variance portfolio starting from Jan 1990 to Dec 2014.
 #' @docType data
 #' @source TBA
@@ -110,7 +139,7 @@ NULL
 
 #' @name mktUS
 #' @title US Market Returns
-#' @description Monthly returns including all distributions, on a value-weighted market portfolio of NYSE/AMEX/NASDAQ 
+#' @description Monthly returns including all distributions, on a value-weighted market portfolio of NYSE/AMEX/NASDAQ
 #' @docType data
 #' @source WRDS
 #' @usage data("mktUS")
@@ -219,7 +248,7 @@ NULL
 
 
 #' @name spxAnnual
-#' @title S&P 500 annual return 
+#' @title S&P 500 annual return
 #' @description S&P 500 annual return, with year end price and total return
 #' @docType data
 #' @source TBA
@@ -227,7 +256,7 @@ NULL
 NULL
 
 #' @name brkAnnual
-#' @title Berkshire Hathaway annual return 
+#' @title Berkshire Hathaway annual return
 #' @description Berkshire Hathaway annual return, with year end price and total return
 #' @docType data
 #' @source TBA
@@ -235,7 +264,7 @@ NULL
 NULL
 
 #' @name brkMonthlyBloomberg
-#' @title Berkshire Hathaway montly return 
+#' @title Berkshire Hathaway montly return
 #' @description Berkshire Hathaway montly return
 #' @docType data
 #' @source Bloomberg
@@ -245,7 +274,7 @@ NULL
 
 #' @name managers
 #' @title Hypothetical Alternative Asset Manager and Benchmark Data
-#' @description This dataset and it's documentation have been duplicated from  \link[PerformanceAnalytics]{managers}} in the \code{PerformanceAnalytics} package. \code{managers} is used in the examples and vignette of the \code{factorAnalytics} package. 
+#' @description This dataset and it's documentation have been duplicated from  \link[PerformanceAnalytics]{managers}} in the \code{PerformanceAnalytics} package. \code{managers} is used in the examples and vignette of the \code{FactorAnalytics} package.
 #' A xts object that contains columns of monthly returns for six hypothetical asset managers (HAM1 through HAM6), the EDHEC Long-Short Equity hedge fund index, the S\&P 500 total returns, and total return series for the US Treasury 10-year bond and 3-month bill. Monthly returns for all series end in December 2006 and begin at different periods starting from January 1996. Note that all the EDHEC indices are available in \code{\link{edhec}}.
 #' @docType data
 #' @source TBA
