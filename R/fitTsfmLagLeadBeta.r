b0c063e3fed4a28b411c4487f5be30fa90954de4
kirk li <cocokecoli@gmail.com>
2021-03-08 23:25:22 -0800

    updates to fitFfm with lmrobdetMM updates to fitTsfm with lmrobdetMM updates to fitTsfmlagBeta with lead

--- R/fitTsfmLagLeadBeta.r
+++ R/fitTsfmLagLeadBeta.r
@@ -1,4 +1,4 @@
-#' @title Fit a lagged Betas factor model using time series regression
+#' @title Fit a lagged and lead Betas factor model using time series regression
 #' 
 #' @description This is a wrapper function to fits a time series lagged Betas factor model for one 
 #' or more asset returns or excess returns using time series regression. 
@@ -7,12 +7,14 @@
 #' \code{"tsfm"} is returned.
 #' 
 #' @details 
-#' The lagged returns model estimates lagged market Beta. Specifically, 
-#' \deqn{r_t = \alpha + \beta_0 MKT_t + \beta_1 MKT_t-1 + \ldots + \beta_K MKT_t-K + \epsilon_t, t=1 \ldots T}
+#' The lagged and lead returns model estimates lagged and lead market Beta. Specifically, 
+#' \deqn{r_t = \alpha + \beta_0 MKT_t + \beta^-_1 MKT_t-1 + \ldots + \beta^-_K+1 MKT_t-K 
+#' +  \beta^+_1 MKT_t+1 + \ldots + \beta^+_K MKT_t+K \epsilon_t, t=1 \ldots T}
 #' where \eqn{r_t} is the asset returns, and MKT is the market factor. It is usually 
 #' needed for illiquid securities with stale prices. One can also report the sum of 
-#' the lagged Betas: 
-#' \deqn{\beta = \beta_0 + \beta_1 + \ldots + \beta_K }  
+#' the lagged and lead Betas: 
+#' \deqn{\beta = \beta_0 + \beta^+_1 + \beta^+_1 + \ldots + \beta^+_K + 
+#' \beta^-_1 + \ldots + \beta^-_K }  
 #' 
 #' @param asset.names vector containing names of assets, whose returns or 
 #' excess returns are the dependent variable.
@@ -21,8 +23,11 @@
 #' @param rf.name name of the column of risk free rate variable to calculate 
 #' excess returns for all assets (in \code{asset.names}) and the market factor (in 
 #' \code{mkt.name}).Default is NULL, and no action is taken.
-#' @param LagBeta A integer number to specify numbers of lags of Betas to 
+#' @param LagLeadBeta A integer number to specify numbers of lags (and leads when LagOnly is FALSE) 
+#' of Betas to 
 #' include in the model. The Default is 1.
+#' @param LagOnly Flag variable to only include the lags (or have both lags and leads). 
+#' The Default is FALSE (both lags and leads). 
 #' @param data vector, matrix, data.frame, xts, timeSeries or zoo object  
 #' containing column(s) named in \code{asset.names}, \code{factor.names} and 
 #' optionally, \code{mkt.name} and \code{rf.name}.
@@ -33,7 +38,7 @@
 #' \code{\link{fitTsfm.control}} for details.
 #' @param ... arguments passed to \code{\link{fitTsfm.control}}
 #' 
-#' @return \code{fitTsfmLagBeta} also returns an object of class \code{"tsfm"} like 
+#' @return \code{fitTsfmLagLeadBeta} also returns an object of class \code{"tsfm"} like 
 #' \code{fitTsfm}. The generic function such as \code{print}, \code{plot}, \code{predict} 
 #' and \code{summary} methods exist. Also, the generic accessor functions \code{coef}, 
 #' \code{fitted}, \code{residuals} and  \code{fmCov} can be applied as well.
@@ -51,7 +56,7 @@
 #' \item{data}{xts data object containing the assets and factors.}
 #' \item{asset.names}{asset.names as input.}
 #' \item{fit.method}{fit.method as input.}
-#' Where N is the number of assets, L is the number of lagged market Betas and T is the 
+#' Where N is the number of assets, L is the number of lagged and lead market Betas and T is the 
 #' number of time periods.
 #' 
 #' @author Yi-An Chen.
@@ -66,18 +71,19 @@
 #' 
 #' @examples
 #' # load data from the database
+#' library(PerformanceAnalytics)
 #' data(managers)
 #' 
 #' # example: A lagged Beetas model with LS fit
-#' fit <- fitTsfmLagBeta(asset.names=colnames(managers[,(1:6)]),LagBeta=2,
-#'                       mkt.name="SP500.TR",rf.name="US.3m.TR",data=managers)
+#' fit <- fitTsfmLagLeadBeta(asset.names=colnames(managers[,(1:6)]),LagLeadBeta=2,LagOnly=TRUE,
+#'                       mkt.name="SP500 TR",rf.name="US 3m TR",data=managers)
 #' summary(fit)
 #' fitted(fit)
 #' 
 #' @export
 
-fitTsfmLagBeta <- function(asset.names, mkt.name, rf.name=NULL, 
-                          data=data, fit.method=c("LS","DLS","Robust"),LagBeta=1, 
+fitTsfmLagLeadBeta <- function(asset.names, mkt.name, rf.name=NULL, 
+                          data=data, fit.method=c("LS","DLS","Robust"),LagLeadBeta=1, LagOnly=FALSE,
                           control=fitTsfm.control(...),...) {
   
   if (is.null(mkt.name))  {
@@ -85,19 +91,24 @@ fitTsfmLagBeta <- function(asset.names, mkt.name, rf.name=NULL,
   }
  
   
-  if (as.integer(LagBeta) != LagBeta | LagBeta < 1 ) {
-    stop("Invalid argument: LagBeta must be an integer and no less than 1. The default is 1.")
+  if (as.integer(LagLeadBeta) != LagLeadBeta | LagLeadBeta < 1 ) {
+    stop("Invalid argument: LagLeadBeta must be an integer and no less than 1. The default is 1.")
   }
   
   # Create market lag terms
   factor.names = mkt.name
-  mktlag <- lag(data[,mkt.name],k=seq(1:LagBeta))
-  for (i in 1:LagBeta) {
+  mktlag <- lag(data[,mkt.name],k=seq(1,LagLeadBeta,1))
+  mktlead <- lag(data[,mkt.name],k=seq(-1,-LagLeadBeta,-1))
+  
+  for (i in 1:LagLeadBeta) {
     colnames(mktlag)[i] <- paste("MktLag",i,sep="")
-    factor.names <- c(factor.names,paste("MktLag",i,sep=""))
+	colnames(mktlead)[i] <- paste("MktLead",i,sep="")
+	
+    factor.names <- c(factor.names,paste("MktLag",i,sep=""),paste("MktLead",i,sep=""))
   }
     data <- merge(data,mktlag)
-  
+	data <- merge(data,mktlead)
+	
   fit <-  fitTsfm(asset.names=asset.names,factor.names=factor.names,mkt.name=mkt.name,rf.name=rf.name,
                   data=data,fit.method=fit.method,variable.selection="none",control=control)
   
