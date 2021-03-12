b25e0a9f7ce1d0ea7894809b9dafc348c56128dc
kirk li <cocokecoli@gmail.com>
2021-03-11 17:48:55 -0800

    add laglead == 0 option

--- R/fitTsfmLagLeadBeta.r
+++ R/fitTsfmLagLeadBeta.r
@@ -85,32 +85,34 @@
 fitTsfmLagLeadBeta <- function(asset.names, mkt.name, rf.name=NULL, 
                           data=data, fit.method=c("LS","DLS","Robust"),LagLeadBeta=1, LagOnly=FALSE,
                           control=fitTsfm.control(...),...) {
-  
-  if (is.null(mkt.name))  {
-    stop("Missing argument: mkt.name has to be specified for lagged Betas model.")
-  }
- 
-  
-  if (as.integer(LagLeadBeta) != LagLeadBeta | LagLeadBeta < 1 ) {
-    stop("Invalid argument: LagLeadBeta must be an integer and no less than 1. The default is 1.")
-  }
-  
-  # Create market lag terms
-  factor.names = mkt.name
-  mktlag <- lag(data[,mkt.name],k=seq(1,LagLeadBeta,1))
-  if(!LagOnly)
-  	mktlead <- lag(data[,mkt.name],k=seq(-1,-LagLeadBeta,-1))
-  
-  for (i in 1:LagLeadBeta) {
-    colnames(mktlag)[i] <- paste("MktLag",i,sep="")
-	factor.names <- c(factor.names,paste("MktLag",i,sep=""))
-	if(!LagOnly)		
-		colnames(mktlead)[i] <- paste("MktLead",i,sep="")
-		factor.names <- c(factor.names,paste("MktLead",i,sep=""))
-  }
-    data <- merge(data,mktlag)
-	if(!LagOnly)		
-		data <- merge(data,mktlead)
+					  
+	if (LagLeadBeta!=0){
+	  if (is.null(mkt.name))  {
+	    stop("Missing argument: mkt.name has to be specified for lagged Betas model.")
+	  }
+	 
+	  
+	  if (as.integer(LagLeadBeta) != LagLeadBeta | LagLeadBeta < 1 ) {
+	    stop("Invalid argument: LagLeadBeta must be an integer and no less than 1. The default is 1.")
+	  }
+	  
+	  # Create market lag terms
+	  factor.names = mkt.name
+	  mktlag <- lag(data[,mkt.name],k=seq(1,LagLeadBeta,1))
+	  if(!LagOnly)
+	  	mktlead <- lag(data[,mkt.name],k=seq(-1,-LagLeadBeta,-1))
+	  
+	  for (i in 1:LagLeadBeta) {
+	    colnames(mktlag)[i] <- paste("MktLag",i,sep="")
+		factor.names <- c(factor.names,paste("MktLag",i,sep=""))
+		if(!LagOnly)		
+			colnames(mktlead)[i] <- paste("MktLead",i,sep="")
+			factor.names <- c(factor.names,paste("MktLead",i,sep=""))
+	  }
+	    data <- merge(data,mktlag)
+		if(!LagOnly)		
+			data <- merge(data,mktlead)
+	}
 	
   fit <-  fitTsfm(asset.names=asset.names,factor.names=factor.names,mkt.name=mkt.name,rf.name=rf.name,
                   data=data,fit.method=fit.method,variable.selection="none",control=control)
