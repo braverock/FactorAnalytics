3ac6290f769ff1228b5d8b56880b5613a8ab3485
JustinMShea <jshea@roosevelt.edu>
2021-07-19 15:19:07 -0500

    #20 refactored for tinytest away from testthat

--- inst/tinytest/test-fitTsfm.R
+++ inst/tinytest/test-fitTsfm.R
@@ -1,11 +1,8 @@
-context("Test fitTsfm")
-require(testthat)
-test_that("fitTsfm is as expected", {
-  
-  fpath <- system.file("extdata", "timeSeriesReturns.csv", 
-                       package="FactorAnalytics")
-  returns.z <- read.zoo(file=fpath, header=TRUE, sep=",", as.is=TRUE,
-                        FUN=as.yearmon)
+
+  #fpath <- system.file("tinytest","timeSeriesReturns.csv", 
+  #                     package="FactorAnalytics")
+  returns.z <- read.zoo(file="timeSeriesReturns.csv", header=TRUE, sep=",", 
+                        as.is=TRUE, FUN=as.yearmon)
   returns.z <- window(returns.z, start="2008-01-01", end="2012-12-31")  
   assets <- names(returns.z)[1:30]
   ex.rets <- returns.z[,assets]-returns.z$rf
@@ -26,8 +23,6 @@ test_that("fitTsfm is as expected", {
                     data=cbind(ex.rets,carhart))
   
   # compare beta and r2
-  expect_that(as.matrix(ff.mod$beta),is_equivalent_to(t(coef(ff4)[-1,])))
-  expect_that(as.numeric(ff.mod$r2), 
-              equals(as.numeric(sapply(X=sum4, FUN="[", "r.squared"))))
+  expect_equivalent(as.matrix(ff.mod$beta), t(coef(ff4)[-1,]))
+  expect_equal(as.numeric(ff.mod$r2), as.numeric(sapply(X=sum4, FUN="[", "r.squared")))
   
-})
