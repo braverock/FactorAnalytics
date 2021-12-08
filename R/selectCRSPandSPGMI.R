5795002886904f3952c8c7a9e2bf91d090d29928
martinrd <martinrd@comcast.net>
2021-12-08 12:16:29 -0800

    New function for selecting a combination of CRSP stocks and SPGMI factors for use by fitFfmDT

---
+++ R/selectCRSPandSPGMI.R
@@ -0,0 +1,43 @@
+#' @title Select CRSP Stocks and SPGMI Factors
+#'
+#' @description Select and merge a set of CRSP stocks and SPGMI factors and
+#' SPGMI factors over a range of dates between January 1993 and December 2015
+#'
+#' @details
+#' The date range is specified by a beginning Date object and an ending Date 
+#' object.  Default values for the stocksCRSP and scoresSPGMI component names,
+#' marketcap group, and number of stocks in the marketcap group can be
+#' changed by the user  
+#' @param stocksCRSP 
+#' @param scoresSPGMI 
+#' @param dateSet 
+#' @param stockItems 
+#' @param factorItems 
+#' @param capChoice 
+#' @param Nstocks 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+selectCRSPandSPGMI <- function(stocksCRSP,scoresSPGMI,
+                               dateSet = c("2006-01-31","2010-12-31"), 
+                               stockItems = c("Date","TickerLast","CapGroup","Sector","Return",
+                                              "Ret13WkBill","mktIndexCRSP"),
+                               factorItems = c("BP","LogMktCap","SEV"), 
+                               capChoice = "SmallCap",
+                               Nstocks = 20)
+{ 
+  commonNames <- intersect(names(stocksCRSP),names(scoresSPGMI))
+  facModDat <- merge(stocksCRSP, scoresSPGMI, by = commonNames)
+  itemsStocks <- stockItems
+  itemsFactors <- factorItems
+  colNames <- c(itemsStocks,itemsFactors)
+  facModDat <- facModDat[, .SD , .SDcols = colNames]
+  facModDat <- facModDat[!is.na(Sector)] # 250 stocks for now
+  facModDat <- facModDat[CapGroup == capChoice]
+  facModDat <-facModDat[Date >= dateSet[1] & Date <= dateSet[2]]
+  tickerSet <- unique(facModDat[,TickerLast])[1:Nstocks]
+  facModDat <- facModDat[TickerLast %in% tickerSet,]
+  facModDat
+}
