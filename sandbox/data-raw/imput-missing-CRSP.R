03aff7a186fd1adbd89d5c527752bdd120e5804b
JustinMShea <jshea@roosevelt.edu>
2021-12-23 21:27:23 -0600

    #67 imputed missing GICS into stocksCRSP

--- sandbox/data-raw/imput-missing-CRSP.R
+++ sandbox/data-raw/imput-missing-CRSP.R
@@ -21,33 +21,35 @@ sum(unique(stocksCRSP$TickerLast) %in% unique(GICS_govind$Ticker))
 setdiff(unique(GICS_govind$Ticker), unique(stocksCRSP$TickerLast))
 
 ##############################################
-# Imput GICS into missing factorsSPGMI$GICS #
+# Imput GICS into missing stocksCRSP$GICS #
 ############################################
 
-SPGMI_noGICS <- factorsSPGMI[is.na(factorsSPGMI$GICS), ]
+CRSP_noGICS <- stocksCRSP[is.na(stocksCRSP$GICS), ]
 # merge on the two variables that are complete
-SPGMI_repair <- merge(x = SPGMI_noGICS, y = GICS_govind, 
+CRSP_repair <- merge(x = CRSP_noGICS, y = GICS_govind, 
                       by.x = c("TickerLast","Company"),
-                      by.y = c("Ticker","Company Name"))
+                      by.y = c("Ticker","Company Name")
+                     )
 
 # test the results of the process for NAs on the right GICS_govind side. 
-sum(is.na(SPGMI_repair$GICS.x)) # should be 13800
-sum(is.na(SPGMI_repair$Sector.x)) # should be 13800
-sum(is.na(SPGMI_repair$GICS.y)) # should be 0
-sum(is.na(SPGMI_repair$Sector.y)) # should be 13524
+sum(is.na(CRSP_repair$GICS.x)) # should be 13800
+sum(is.na(CRSP_repair$Sector.x)) # should be 13800
+sum(is.na(CRSP_repair$GICS.y)) # should be 0
+sum(is.na(CRSP_repair$Sector.y)) # should be 13524
 
 # drop merged GICS.x and Sector.x of all NAs
-SPGMI_repair <- SPGMI_repair[,-c("GICS.x","Sector.x")]
+CRSP_repair <- CRSP_repair[,-c("GICS.x","Sector.x")]
 # rename GICS.y and Sector.y to GICS and Sector
-colnames(SPGMI_repair)[which(colnames(SPGMI_repair) =='GICS.y')] <- "GICS"
-colnames(SPGMI_repair)[which(colnames(SPGMI_repair) =='Sector.y')] <- "Sector"
-# With formatted columns, now row bind onto factorsSPGMI data that is complete
-SPGMI_allGICS <- factorsSPGMI[!is.na(factorsSPGMI$GICS), ]
-SPGMI_complete <- rbind(SPGMI_allGICS, SPGMI_repair, fill=TRUE)
-# Return names and order or original factorsSPGMI state
+colnames(CRSP_repair)[which(colnames(CRSP_repair) =='GICS.y')] <- "GICS"
+colnames(CRSP_repair)[which(colnames(CRSP_repair) =='Sector.y')] <- "Sector"
+# With formatted columns, now row bind onto stocksCRSP data that is complete
+CRSP_allGICS <- stocksCRSP[!is.na(stocksCRSP$GICS), ]
+CRSP_complete <- rbind(CRSP_allGICS, CRSP_repair, fill=TRUE)
+# Return names and order or original stocksCRSP state
 # drop "GICS Descriptor" "Start date" "End Date"  
-SPGMI_complete <- SPGMI_complete[,-c("GICS Descriptor","Start date","End Date")]
+CRSP_complete <- CRSP_complete[,-c("GICS Descriptor","Start date","End Date")]
 
 # test the results of the process for NAs #
-apply(SPGMI_complete, 2, function(x) sum(is.na(x)))
+apply(CRSP_complete, 2, function(x) sum(is.na(x)))
 # Results: 0 missing values except "Sector", which has 13524
+
